882527772340ab927b41a4a30da13a3987ce2eba
targs08 <taras.udm@gmail.com>
2019-03-04 14:12:56 +0400

    Corrections

--- src/Command/UpdatePlatformReqsCommand.php
+++ src/Command/UpdatePlatformReqsCommand.php
@@ -2,28 +2,42 @@
 
 namespace Instrumentisto\Composer\Command;
 
-use Composer\Factory;
+
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Composer\Command\BaseCommand;
 
+use Composer\Command\BaseCommand;
+use Composer\Factory;
 use Composer\Json\JsonManipulator;
 use Composer\Repository\PlatformRepository;
 
 
+/**
+ * Generate platform requirements
+ */
 class UpdatePlatformReqsCommand extends BaseCommand
 {
     protected function configure()
     {
         $this
             ->setName('update-platform-reqs')
-            ->setDescription('Update platform requirements in composer.json');
+            ->setDescription('Update platform requirements in composer.json')
+            ->setDefinition(array(
+                new InputOption('output-console', null,
+                    InputOption::VALUE_NONE, '
+                    Don\'t update composer.json, only output to console')
+            ))
+            ->setHelp(
+                <<<EOT
+Generate platform requirements and build structure for config.platform options
+EOT
+            );
+
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Updating config.platform');
-
         $content = file_get_contents(Factory::getComposerFile());
         $manipulator = (new JsonManipulator($content));
         $manipulator->removeSubNode('config', 'platform');
@@ -41,8 +55,14 @@ class UpdatePlatformReqsCommand extends BaseCommand
         foreach ($extensions as $name => $version) {
             $manipulator->addSubNode('config', 'platform.'.$name, $version);
         }
-        file_put_contents(Factory::getComposerFile(), $manipulator->getContents());
 
+        if ($input->getOption('output-console')) {
+            $output->writeln(json_encode($extensions, JSON_PRETTY_PRINT));
+        } else {
+            file_put_contents(Factory::getComposerFile(),
+                                            $manipulator->getContents());
+            $output->writeln('Updated config.platform');
+        }
     }
 
 }
